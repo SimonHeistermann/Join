e55e28efc7ba03a1ce8ad3518a7f04432f4bcd96
Simon Heistermann <simon.heistermann@icloud.com>
2025-01-17 13:02:12 +0100

    updated login and startet with sign up page

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,45 @@
+let skipAnimation;
+
+function initLogin() {
+    logoAnimation(false);
+}
+
+function logoAnimation(skipAnimation) {
+    const overlay = document.getElementById('overlay');
+    const bigLogoDiv = document.getElementById('biglogo_div');
+    if (skipAnimation) {
+        skipLogoAnimation(overlay, bigLogoDiv);
+    } else {
+        startLogoAnimation(overlay, bigLogoDiv);
+    }
+}
+
+function skipLogoAnimation(overlay, bigLogoDiv) {
+    overlay.classList.add("d__none");  
+    bigLogoDiv.classList.add("d__none");  
+}
+
+function startLogoAnimation(overlay, bigLogoDiv) {
+    setTimeout(() => {
+        overlay.classList.add("hidden");
+    }, 500); 
+    setTimeout(() => {
+        bigLogoDiv.classList.add("move__to__corner");
+    }, 100);
+    endLogoAnimation(overlay, bigLogoDiv);
+}
+
+function endLogoAnimation(overlay, bigLogoDiv) {
+    setTimeout(() => {
+        overlay.classList.add("d__none");  
+        bigLogoDiv.classList.add("d__none");  
+    }, 1000);
+}
+
 function maskPassword(input) {
-    const actualValue = input.value;  // Der tatsächliche Wert
-    input.dataset.actualValue = actualValue;  // Speichere den tatsächlichen Wert im Dataset
-    input.value = '*'.repeat(actualValue.length);  // Zeige Sterne (oder ein anderes Zeichen)
+    const actualValue = input.value;  
+    input.dataset.actualValue = actualValue;  
+    input.value = '*'.repeat(actualValue.length); 
 }
 
 
