cc17888319e8ae44591458133c56125a8d8e6a50
Eid Aden <e-aden@hotmail.de>
2025-01-23 13:03:17 +0100

    Update

---
+++ scripts/board.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("popup").style.display = "block";
+  console.log("Hello World");
+}
+
+function closeNav() {
+  document.getElementById("popup").style.display = "none";
+}
