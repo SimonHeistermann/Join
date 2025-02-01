b4b6832744f2d1bd9e8b73f87ed536d705324238
Eid Aden <e-aden@hotmail.de>
2025-02-01 10:04:23 +0100

    commitm

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,1 +1,59 @@
+async function saveTask() {
+  const name = document.getElementById("text__input").value;
+  const description = document.getElementById("description").value;
+  const assignedTo = document.getElementById("assigned__to").value;
+  const dueDate = document.getElementById("date__input").value;
+  const prio = document.querySelector('input[name="prio"]:checked')?.value;
+  const category = document.querySelector('select[name="category"]').value;
+  const subtask = document.getElementById("subtask").value;
 
+  if (!name || !description || !assignedTo || !dueDate || !prio || !category) {
+    document.getElementById("star_date").innerHTML =
+      "Bitte alle Felder ausfüllen!";
+    document.getElementById("star_textArea").innerHTML =
+      "Bitte alle Felder ausfüllen!";
+    document.getElementById("star_title").innerHTML =
+      "Bitte alle Felder ausfüllen!";
+    return;
+  }
+
+  const taskData = {
+    name: name,
+    description: description,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    prio: prio,
+    category: category,
+    subtask: subtask,
+  };
+
+  try {
+    let response = await fetch(baseUrl + "tasks.json", {
+      method: "POST", // Neues Task-Objekt in Firebase hinzufügen
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+
+    if (response.ok) {
+      console.log("Task gespeichert!");
+      fetchData(); // Liste neu laden
+      clearForm();
+    } else {
+      console.error("Fehler beim Speichern!");
+    }
+  } catch (error) {
+    console.error("Fehler:", error);
+  }
+}
+
+function clearForm() {
+  document.getElementById("text__input").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("assigned__to").value = "";
+  document.getElementById("date__input").value = "";
+  document.querySelector('input[name="prio"]:checked').checked = false;
+  document.querySelector('select[name="category"]').value = "";
+  document.getElementById("subtask").value = "";
+}
