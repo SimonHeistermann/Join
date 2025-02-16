5e38912be90c042de7afd93f8b0874364d3f5b4e
Eid Aden <e-aden@hotmail.de>
2025-02-16 20:45:46 +0100

    com

--- scripts/board.js
+++ scripts/board.js
@@ -182,6 +182,52 @@ function createPopup(task) {
       </div>
     </div>
   </div>
+
+
+ <div class="mobile__responsive">
+      <div class="popup__card-header">
+        <div class="close__btn__popup">
+          <span class="task__type user__story">${task.category}</span>
+          <button onclick="closePopup()">&times;</button>
+        </div>
+      </div>
+      <div class="all__content">
+        <div class="title_header_respo">${task.name}</div>
+        <div class="popup__card-section">
+          <p>${task.description}</p>
+        </div>
+        <div class="popup__card-section">
+          <p><strong>Due date:</strong> ${
+            task.due_date || "Kein Datum angegeben"
+          }</p>
+        </div>
+        <p><strong>Priority:</strong>
+         <span class="popup__card-priority">${priority}</span></p>
+      </div>
+      <div class="popup__card-section">
+        <p><strong>Assigned To:</strong> ${assignedTo}</p>
+        <div class="popup__card-section">
+          <p><strong>Subtasks:</strong> ${
+            task.subtasks ? task.subtasks.join(", ") : "Keine Subtasks"
+          }</p>
+        </div>
+      </div>
+      <div class="conten__delete__editiBTN">
+        <div class="popup__card-actions">
+         <button class="delete-btn" onclick="deleteTask('${
+           task.id ? task.id : ""
+         }')">
+
+            <img src="assets/icons/delete_icon_blue.png" alt=""> Delete
+          </button>
+          <button id="popup_edit_button" class="edit_button">
+            <img src="assets/icons/edit_icon_blue.png" alt="">Edit
+          </button>
+        </div>
+      </div>
+    </div>
+
+
   `;
 }
 
