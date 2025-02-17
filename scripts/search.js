2b42b72497479c11c2fdf9bd70635831e4c4a627
Eid Aden <e-aden@hotmail.de>
2025-02-17 10:00:45 +0100

    com

--- scripts/search.js
+++ scripts/search.js
@@ -1,19 +1,52 @@
 function searchTasks() {
-  let searchInput = document.getElementById("find_task");
-  let searchText = searchInput.value.toLowerCase();
-  let tasks = document.getElementsByClassName("task");
+  let searchInput = document.getElementById("searchInput").value.toLowerCase();
+  let taskContainer = document.getElementById("content");
 
-  for (let i = 0; i < tasks.length; i++) {
-    let task = tasks[i];
-    let taskName = task.getElementsByTagName("h3")[0].innerText.toLowerCase();
-    let taskDescription = task
-      .getElementsByTagName("p")[0]
-      .innerText.toLowerCase();
+  taskContainer.innerHTML = "";
 
-    if (taskName.includes(searchText) || taskDescription.includes(searchText)) {
-      task.style.display = "block"; // Zeigt die passende Aufgabe an
-    } else {
-      task.style.display = "none"; // Versteckt die nicht passenden Aufgaben
-    }
-  }
+  fetch(`${baseUrl}/tasks.json`)
+    .then((response) => response.json())
+    .then((tasks) => {
+      if (!tasks) {
+        tasks = {};
+      }
+
+      let taskIds = Object.keys(tasks);
+      let filteredTasks = taskIds.filter((taskId) => {
+        let task = tasks[taskId];
+        return (
+          task.name.toLowerCase().includes(searchInput) ||
+          (task.description &&
+            task.description.toLowerCase().includes(searchInput))
+        );
+      });
+
+      if (filteredTasks.length === 0) {
+        taskContainer.innerHTML = "<p>No tasks</p>";
+      } else {
+        filteredTasks.forEach((taskId) => {
+          let task = tasks[taskId];
+          taskContainer.innerHTML += callbackCode(taskId, task);
+        });
+      }
+    });
+}
+
+function callbackCode(taskId, task) {
+  return `
+    <div id="task${taskId}" class="task" draggable="true" ondragstart="drag(event)">
+      <div class="Overlay" onclick='showPopup(${JSON.stringify(task)})'>
+        <div class="task-type">Category</div>
+        <h3>${task.name}</h3>
+        <p>${task.description}</p>
+        <div class="progress">
+          <div class="progress-bar" style="width: ${task.progress || 2}%"></div>
+        </div>
+        <div class="subtasks">
+          1/2 
+          <p>${task.subtasks ? task.subtasks.join(", ") : ""}</p> Subtasks
+        </div>
+      </div>
+    </div>
+  `;
 }
